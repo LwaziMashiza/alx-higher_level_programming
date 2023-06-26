69eb8f00f2ce3f3d250414601059896ec3d1c735
Mbuso <lwaziluthuli0@gmail.com>
2023-06-26 09:54:14 -0700

    file 2 completed

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,11 @@
-afe_print_division(a, b):
-    try:
-        result = a / b
-    except:
-        result = None
-    finally:
-        print("Inside result: {}".format(result))
-        return (result)
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count = count + 1
+        except (TypeError, ValueError):
+            pass
+    print("")
+    return count
