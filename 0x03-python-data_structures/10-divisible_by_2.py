f5dd2e9e3c158e8527fab2f2263dcc3c0a83ec0a
root <lwaziluthuli0@gmail.com>
2023-08-14 11:42:02 -0700

    10-divisible_by_2 file completed

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+sible_by_2(my_list=[]):
+        if my_list is None:
+                    return
+                    newList = []
+                        for i in my_list:
+                                    if i % 2 == 0:
+                                                    newList.append(True)
+                                                            else:
+                                                                            newList.append(False)
+                                                                                return newList
