9f49e7556b20cc6b09018ae52e3531273253af52
root <lwaziluthuli0@gmail.com>
2023-08-16 13:59:49 -0700

    8-simple_delete file completed

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
+def simple_delete(my_dict, key=""):
+    if key in my_dict:
+        del my_dict[key]
+    return my_dict
